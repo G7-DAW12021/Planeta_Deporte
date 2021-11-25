a286a6cbb34d0edf2abe8dea46f696e139b0c453
JuanjoBS00 <60982868+JuanjoBS00@users.noreply.github.com>
2021-11-25 18:27:39 +0000

    Actualización V2: Sprint 2 Final

--- scripts/comment_maker.js
+++ scripts/comment_maker.js
@@ -5,9 +5,17 @@ $(document).ready(function(){
 
         function make_comment(){
             $(".add_comment_button").hide();
-            var commentArea = $('<textarea  id="comentArea" class="add_comment_txt" type="text" required rows="10px" cols="80px" > </textarea>');
-            var submitButton = $('<button id ="add_hidden_btn" class="add_comment_button" name="add_btn_hid" type="button">Agregar</button>');
-            var cancelButton = $('<button id ="cancel_hidden_btn" class="add_comment_button" name="cancel_btn_hid" type="button">Cancelar</button>');
+            
+            var submitButton = $('<button id ="add_hidden_btn" class="add_comment_button" name="add_btn_hid" type="button">Agregar comentario</button>');
+            var cancelButton = $('<button id ="cancel_hidden_btn" class="cancel_comment_button" name="cancel_btn_hid" type="button">Cancelar comentario</button>');
+
+            if(window.innerWidth > 991){
+                var commentArea = $('<textarea  id="comentArea" class="add_comment_txt" type="text" required rows="10px" cols="80px" > </textarea>');
+            }else if(window.innerWidth > 767){
+                var commentArea = $('<textarea  id="comentArea" class="add_comment_txt" type="text" required rows="7px" cols="60px" > </textarea>');
+            }else{
+                var commentArea = $('<textarea  id="comentArea" class="add_comment_txt" type="text" required rows="5px" cols="45px" > </textarea>');
+            }
 
             $("#text_area_div").append(commentArea);
             $("#submit_button_div").append(submitButton);
